4f4ee91853dcaa55b23115adb76711b1c39fef79
«Mary <isabarling@gmail.com>
2022-08-07 13:31:31 +0300

    отладка

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int userNumber = new int();
-Console.WriteLine("Введите число: ");
-userNumber = Convert.ToInt32(Console.ReadLine());
-
-
-int result = (userNumber * userNumber);
-Console.WriteLine(result);
-
